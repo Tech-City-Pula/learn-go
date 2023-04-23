83b9f0f6da4724d823f0a2121f86ef2ae23567c7
Domogo <domsatrapa@gmail.com>
2023-04-23 12:37:51 +0200

    vars

---
+++ variables/variables.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial"
+	fmt.Println(a)
+
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	var d = true
+	fmt.Println(d)
+
+	var e int
+	fmt.Println(e)
+
+	// := syntax is shorthand for declaring and initializing a variable, only available inside a function
+	f := "apple"
+	fmt.Println(f)
+}
