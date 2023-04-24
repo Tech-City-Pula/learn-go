ef2a0b1a295a4406b66fec15333dca97799dddad
Domogo <domsatrapa@gmail.com>
2023-04-24 12:27:44 +0200

    struct embed, generics, errors, goroutines, channels

---
+++ examples/channel-buffering/channel-buffering.go
@@ -0,0 +1,15 @@
+// https://gobyexample.com/channel-buffering
+package main
+
+import "fmt"
+
+func main() {
+
+	messages := make(chan string, 2)
+
+	messages <- "buffered"
+	messages <- "channel"
+
+	fmt.Println(<-messages)
+	fmt.Println(<-messages)
+}
