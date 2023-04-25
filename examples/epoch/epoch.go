e5a6853ec0c1216af8319cb06c21c051c0823ae6
Domogo <domsatrapa@gmail.com>
2023-04-25 14:08:39 +0200

    text templates, regex, json, xml, time, epoch, time formatting, rand, number parse, url parse, sha256, base64, r/w

---
+++ examples/epoch/epoch.go
@@ -0,0 +1,20 @@
+// https://gobyexample.com/epoch
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+
+	now := time.Now()
+	fmt.Println(now)
+
+	fmt.Println(now.Unix())
+	fmt.Println(now.UnixMilli())
+	fmt.Println(now.UnixNano())
+
+	fmt.Println(time.Unix(now.Unix(), 0))
+	fmt.Println(time.Unix(0, now.UnixNano()))
+}
---
+++ examples/sha256-hashes/sha256-hashes.go
@@ -0,0 +1,20 @@
+// https://gobyexample.com/sha256-hashes
+package main
+
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+func main() {
+	s := "sha256 this string"
+
+	h := sha256.New()
+
+	h.Write([]byte(s))
+
+	bs := h.Sum(nil)
+
+	fmt.Println(s)
+	fmt.Printf("%x\n", bs)
+}
